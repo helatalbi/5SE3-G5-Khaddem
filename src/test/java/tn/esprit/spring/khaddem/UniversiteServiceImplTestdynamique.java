9f00974c41e69b04c58a2a9c4d6df636872092e5
rimeneguediche <rimene.guediche@esprit.tn>
2023-10-21 15:23:06 +0100

    test sans mockito

--- src/test/java/tn/esprit/spring/khaddem/UniversiteServiceImplTestdynamique.java
+++ src/test/java/tn/esprit/spring/khaddem/UniversiteServiceImplTestdynamique.java
@@ -1,4 +1,49 @@
+package tn.esprit.spring.khaddem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.khaddem.entities.Departement;
+import tn.esprit.spring.khaddem.entities.Universite;
+import tn.esprit.spring.khaddem.repositories.DepartementRepository;
+import tn.esprit.spring.khaddem.repositories.UniversiteRepository;
+import tn.esprit.spring.khaddem.services.UniversiteServiceImpl;
+
+import javax.transaction.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
-class UniversiteServiceImplTestdynamique {
-  
+
+
+@SpringBootTest
+public class UniversiteServiceImplTestdynamique {
+
+    @Autowired
+    private UniversiteServiceImpl universiteService;
+
+    @Autowired
+    private UniversiteRepository universiteRepository;
+
+    @Autowired
+    private DepartementRepository departementRepository;
+
+    @Test
+    public void testUpdateUniversite() {
+
+        Universite universite = new Universite();
+        universite.setNomUniv("Université de Test");
+
+        Universite savedUniversite = universiteService.addUniversite(universite);
+
+        savedUniversite.setNomUniv("Nouveau Nom");
+        Universite updatedUniversite = universiteService.updateUniversite(savedUniversite);
+
+        Universite fetchedUniversite = universiteRepository.findById(updatedUniversite.getIdUniversite()).get();
+
+        assertNotNull(updatedUniversite);
+        assertEquals("Nouveau Nom", updatedUniversite.getNomUniv());
+        assertNotNull(fetchedUniversite);
+        assertEquals("Nouveau Nom", fetchedUniversite.getNomUniv());
+    }
+
+
 }
