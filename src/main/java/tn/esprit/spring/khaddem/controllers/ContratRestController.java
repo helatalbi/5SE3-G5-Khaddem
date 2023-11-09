07b7f6ec5e22d4e2c4c7405091eeff7d9bb1270b
Oussama_Lachiheb <93344901+Oussama-Lachiheb@users.noreply.github.com>
2023-11-09 19:19:14 +0100

    Update ContratRestController.java

--- src/main/java/tn/esprit/spring/khaddem/controllers/ContratRestController.java
+++ src/main/java/tn/esprit/spring/khaddem/controllers/ContratRestController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/contrat")
+@CrossOrigin(value = "*")
 public class ContratRestController {
     @Autowired
     IContratService contratService;
